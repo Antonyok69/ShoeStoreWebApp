7b2cec147193f683152281708af2d816a5a04d2a
Antonyok69 <onyokvelasco2002@gmail.com>
2026-05-16 12:27:35 +0800

    Added API routes for POS integration

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\ProductApiController;
+use App\Http\Controllers\Api\OrderApiController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +19,22 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+/*
+|--------------------------------------------------------------------------
+| Product API Routes
+|--------------------------------------------------------------------------
+*/
+
+Route::get('/products', [ProductApiController::class, 'index']);
+Route::post('/products', [ProductApiController::class, 'store']);
+Route::put('/products/{id}', [ProductApiController::class, 'update']);
+Route::delete('/products/{id}', [ProductApiController::class, 'destroy']);
+
+/*
+|--------------------------------------------------------------------------
+| POS Sales Route
+|--------------------------------------------------------------------------
+*/
+
+Route::post('/pos/sale', [OrderApiController::class, 'storeSale']);
