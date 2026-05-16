72da653b3339b00443279ba960e40944673c63e5
Antonyok69 <onyokvelasco2002@gmail.com>
2026-05-16 13:01:47 +0800

    Connected product API to Shoe model

--- app/Http/Controllers/Api/ProductApiController.php
+++ app/Http/Controllers/Api/ProductApiController.php
@@ -3,9 +3,36 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Shoe;
 use Illuminate\Http\Request;
 
 class ProductApiController extends Controller
 {
-    //
+    public function index()
+    {
+       return response()->json(Shoe::all());
+    }
+
+    public function store(Request $request)
+    {
+        return response()->json([
+            'message' => 'Product store API working'
+        ]);
+    }
+
+    public function update(Request $request, $id)
+    {
+        return response()->json([
+            'message' => 'Product update API working',
+            'id' => $id
+        ]);
+    }
+
+    public function destroy($id)
+    {
+        return response()->json([
+            'message' => 'Product delete API working',
+            'id' => $id
+        ]);
+    }
 }
