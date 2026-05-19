79f94409d1775a3f647928c4292739a8f3f4d5c4
Antonyok69 <onyokvelasco2002@gmail.com>
2026-05-19 19:01:24 +0800

    Added order API controller

--- app/Http/Controllers/Api/OrderApiController.php
+++ app/Http/Controllers/Api/OrderApiController.php
@@ -4,8 +4,15 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Order;
 
 class OrderApiController extends Controller
 {
-    //
+    public function index()
+    {
+        return response()->json([
+            'success' => true,
+            'orders' => Order::latest()->get()
+        ]);
+    }
 }
