5a3ab918aa700f575904ddd9b54687bb8a656503
Antonyok69 <onyokvelasco2002@gmail.com>
2026-05-20 23:20:46 +0800

    Simplified order confirmation page

--- resources/views/Cart/orderConfirmation.blade.php
+++ resources/views/Cart/orderConfirmation.blade.php
@@ -8,38 +8,13 @@
         </div>
 
         <div class="card-body">
-            @if (session('success'))
-                <div class="alert alert-success">
-                    {{ session('success') }}
-                </div>
-            @endif
+            <div class="alert alert-success">
+                Payment successful! Thank you for your purchase.
+            </div>
 
-            <h4>Your Order Summary</h4>
-
-            <table class="table table-bordered mt-3">
-                <thead class="thead-dark">
-                    <tr>
-                        <th>Product</th>
-                        <th>Quantity</th>
-                        <th>Price</th>
-                        <th>Total</th>
-                        <th>Delivery Address</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach ($latestOrders as $order)
-                        <tr>
-                            <td>{{ $order['product_name'] }}</td>
-                            <td>{{ $order['quantity'] }}</td>
-                            <td>${{ number_format($order['price'], 2) }}</td>
-                            <td>${{ number_format($order['total'], 2) }}</td>
-                            <td>{{ $order['address'] }}</td>
-                        </tr>
-                    @endforeach
-                </tbody>
-            </table>
-
-           <a href="{{ route('shoes.index') }}" class="btn btn-primary mt-3">Continue Shopping</a>
+            <a href="{{ route('orders.my') }}" class="btn btn-primary mt-3">
+                View My Orders
+            </a>
         </div>
     </div>
 </div>
